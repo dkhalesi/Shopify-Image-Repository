a2bf64f52628cd06b9b83a645e2936bdbc7cef5b
Dina Khalesi <dinakhalesit@gmail.com>
2021-01-17 01:29:57 -0500

    cleaned up ui for upload and search

--- frontend/src/components/Dashboard.vue
+++ frontend/src/components/Dashboard.vue
@@ -10,23 +10,72 @@
       <v-btn icon>
         <v-icon>mdi-magnify</v-icon>
       </v-btn>
-      <v-file-input
-        label="File input"
-        filled
-        multiple
-        prepend-icon="mdi-camera"
-        @change="selectPicture"
-      ></v-file-input>
-      <v-btn icon v-on:click="uploadPicture()">
-        <v-icon>mdi-cloud-upload</v-icon>
-      </v-btn>
+
       <v-btn icon v-on:click="logout()">
         <v-icon>mdi-logout</v-icon>
       </v-btn>
     </v-app-bar>
 
+    <v-dialog v-model="uploadDialog" max-width="500px">
+      <v-card>
+        <v-card-title> Select Picture </v-card-title>
+        <v-card-text>
+          <v-file-input
+            label="File input"
+            filled
+            multiple
+            prepend-icon="mdi-camera"
+            @change="selectPicture"
+          ></v-file-input>
+          <v-btn icon v-on:click="uploadPicture()">
+            <v-icon>mdi-cloud-upload</v-icon>
+          </v-btn>
+          <v-card-text> Click to upload! </v-card-text>
+        </v-card-text>
+        <v-card-actions>
+          <v-btn text @click="uploadDialog = false"> Close </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
+    <v-dialog v-model="searchDialog" max-width="500px">
+      <v-card>
+        <v-card-title> Search Characteristics: </v-card-title>
+        <v-card-text>
+          <v-toolbar>
+            <v-text-field
+              hide-details
+              prepend-icon="mdi-magnify"
+              single-line
+            ></v-text-field>
+          </v-toolbar>
+        </v-card-text>
+        <v-card-actions>
+          <v-btn text @click="searchDialog = false"> Close </v-btn>
+          <v-btn text @click="searchDialog = false"> View all </v-btn>
+          <v-btn text @click="searchDialog = false"> Confirm</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
     <v-main>
-      <v-container v-if="reversedImages.length > 0">
+      <v-btn
+        color="#96bf48"
+        class="ma-2 white--text"
+        fab
+        @click="uploadDialog = true"
+      >
+        <v-icon dark> mdi-cloud-upload </v-icon>
+      </v-btn>
+      <v-btn
+        color="#96bf48"
+        class="ma-2 white--text"
+        fab
+        @click="searchDialog = true"
+      >
+        <v-icon dark> mdi-magnify </v-icon>
+      </v-btn>
+      <v-container>
         <v-row>
           <v-col
             v-for="(image, n) in reversedImages"
@@ -51,7 +100,6 @@
           </v-col>
         </v-row>
       </v-container>
-      <v-container v-else> Upload! </v-container>
     </v-main>
   </v-app>
 </template>
@@ -74,6 +122,8 @@ export default {
     return {
       currentFile: null,
       reversedImages: this.images.slice().reverse(),
+      uploadDialog: false,
+      searchDialog: false,
     };
   },
   methods: {
